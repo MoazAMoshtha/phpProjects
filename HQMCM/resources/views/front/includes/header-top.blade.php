b769e89d7b2620a08529cb3a57136c71d32dafbe
Moaz A.Moshtha <mama.moshtha@gmail.com>
2020-07-11 21:19:01 +0300

    11/7/2020

---
+++ HQMCM/resources/views/front/includes/header-top.blade.php
@@ -0,0 +1,17 @@
+
+<header>
+
+    <div id="header">
+       <div class="container">
+           <div class="row">
+               <div class="col-xl-12">
+                   <img src="assets/images/backgrand.png" >
+
+
+               </div>
+           </div>
+
+       </div>
+
+   </div>
+</header>
